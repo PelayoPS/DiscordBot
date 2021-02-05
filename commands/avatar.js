0b359b44d051110591550fd8c77298adbb63c280
PelayoPS <pelayops1041@gmail.com>
2021-02-05 15:38:30 +0100

    Actualización del comando avatar

--- commands/avatar.js
+++ commands/avatar.js
@@ -3,7 +3,7 @@ exports.run = async (client, message, args) => {
   /**
    * Comando avatar que sirve para ver la foto de perfil de un usuario
    */
-  let user = message.mentions.users.first() || message.author;//Busca si una persona fue mencionada, en caso contrario usa el autor del mensaje
+  let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;//Busca si una persona fue mencionada, en caso contrario usa el autor del mensaje
   let embed = new Discord.MessageEmbed();//Constructor para el mensaje con formato
   embed.setColor("RANDOM")//Hace que la barra de color cambie cada vez que se pone
     .setDescription("Avatar del usuario: " + `<@!${user.id}>`)//Menciona al usuario del que se muestra el avatar

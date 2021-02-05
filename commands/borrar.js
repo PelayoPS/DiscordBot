d458168d993ab9e578854ba25a1ddddff1121457
PelayoPS <pelayops1041@gmail.com>
2021-02-05 23:30:33 +0100

    Dejo para luego el comando borrar

--- commands/borrar.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-exports.run = async (client, message, args) => {
-    //*Falta por implementar*
-    /**
-     * Comando borrar que sirve para elminar una cantidad de mensajes
-     */
-
-}

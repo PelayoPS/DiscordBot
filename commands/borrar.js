2c939ef9f200c0f3b9466ad3098373ac137b472f
PelayoPS <pelayops1041@gmail.com>
2021-02-07 17:59:17 +0100

    añadido comando borrar con parámetros

---
+++ commands/borrar.js
@@ -0,0 +1,6 @@
+const Discord = require('discord.js');
+exports.run = async (client, message, args) => {
+    const limit = args[0] + 1;
+    await message.delete();
+    message.channel.bulkDelete(limit);
+}

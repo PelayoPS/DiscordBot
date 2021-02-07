fe2d4d7e43b8a95f69c61c196b1b013c1a693034
PelayoPS <pelayops1041@gmail.com>
2021-02-07 16:34:24 +0100

    Mejora del comando ayuda y sus versiones con parámetros

--- commands/help.js
+++ commands/help.js
@@ -1,22 +1,62 @@
 const Discord = require('discord.js');
 exports.run = async (client, message, args) => {
-    /**
-     * Hace un embed message que luego comenta para lo cual utiliza una promesa, con mensaje normal no dejaba porque reaccionaba al mío
-     * se pueden poner imágenes y personalizar mucho más, pero eso lo tengo que mirar para el comando de youtube y el resto de redes
-     * !Cuando se quiera añadir un comando la descripción se deberá incluir aquí!
-     * *Mejorar la presentación final de comando help*
-     */
-    let descrption = "!avatar @user: muestra la foto de perfil de un usuario.\n" +
-                     "!borrar númeroDeMensajes: se utiliza para eliminar mensajes.\n" +
-                     "!botServersInfo: imprime en consola la información relativa a los servidores donde esté el bot.\n" +
-                     "!buenos días/noches/tardes/'': hace un saludo cordial al usuario por hora del día.\n" +
-                     "!decir idCanal mensaje: repite el mensaje en el canal especificado.\n" +
-                     "!help: da información relevante de todos los comandos.\n" +
-                     "!poll pregunta: hace una encuesta de sí o no para responder.\n" +
-                     "!social red: muestra mi id en cada red social o el link a la cuenta.";
+
+    let descrption = "";
+    switch (args[0]) {
+        case "avatar":
+            descrption = "avatar {@user}: muestra la foto de perfil de un usuario. " +
+                "En caso de no tener argumento usa el del autor del mensaje y acepta ids y @usuario.";
+            break;
+        case "botServerInfo":
+            descrption = "comando privado para el desarrollador del bot que sólo hace cosas en consola así que si lo usas no verás el resultado. ";
+            break;
+        case "buenos":
+            descrption = "saluda educadamente al usuario según la hora del día y si se pasan parámetros como días/noches/tardes/ responde con lo correspondiente.";
+            break;
+        case "decir":
+            descrption = "decir {idCanal}: manda un mensaje  al canal que se le mande.";
+            break;
+        case "help":
+            descrption = "proporciona ayuda general del los distintos comandos. ";
+            break;
+        case "poll":
+            descrption = "Saluda educadamente al usuario según la hora del día y si se pasan parámetros como días/noches/tardes/ responde con lo correspondiente.";
+            break;
+        case "social":
+            descrption = "se pueden pasar como parámetros las siguientes redes para conseguir el link de cada una de ellas: \n"+
+            "*instagram* \n"+
+            "*youtube* \n"+
+            "*twitch* \n"+
+            "*battlenet* \n"+
+            "*epic* \n"+
+            "*steam* \n"+
+            "*uplay* \n"+
+            "*spotify* \n"+
+            "*riot*";
+            break;
+        default:
+            descrption = "**!avatar @user**: muestra la foto de perfil de un usuario.\n" +
+                "**!botServersInfo**: imprime en consola la información relativa a los servidores donde esté el bot.\n" +
+                "**!buenos días/noches/tardes/**: hace un saludo cordial al usuario por hora del día.\n" +
+                "**!decir idCanal mensaje**: repite el mensaje en el canal especificado.\n" +
+                "**!help**: da información relevante de todos los comandos.\n" +
+                "***!poll pregunta**: hace una encuesta de sí o no para responder.\n" +
+                "**!social red**: muestra mi id en cada red social o el link a la cuenta.";
+    }
+
+
+    let title = "";
+    if (args[0] === undefined) {
+        title = "Comandos disponibles:";
+    } else {
+        title = "Comando " + args[0] + ":";
+    }
+
     let embedMessage = new Discord.MessageEmbed()//Crea un nuevo mensaje mucho más personalizable que los mensajes base
-        .setTitle("Comandos disponibles:")//Asigna un título
+        .setTitle(title)//Asigna un título
         .setDescription(descrption)//muestra los comandos disponibles
         .setColor('RANDOM');//Cambia el color de la barrera que sale al lado del mensaje
     message.channel.send(embedMessage);
+
+
 }

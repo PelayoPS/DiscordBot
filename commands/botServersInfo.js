f5a81c8f021e8e10c495a4f27cece0524cae0be2
PelayoPS <pelayops1041@gmail.com>
2021-02-09 20:13:52 +0100

    nada

--- commands/botServersInfo.js
+++ commands/botServersInfo.js
@@ -4,5 +4,5 @@ exports.run = async (client, message, args) => {
      * Muestra en consola toda la información relativa a los servidores en los que se encuentra el bot
      * !Sólo yo desde consola lo puedo ver!
      */
-    bot.guilds.cache.forEach((guild) => {console.log(guild)});
+    client.guilds.cache.forEach((guild) => {console.log(guild)});
 }

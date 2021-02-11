ae9ed8399b85851f1a18d4553a9afa344ce8e6ec
PelayoPS <pelayops1041@gmail.com>
2021-02-11 21:03:25 +0100

    Añadido comando invitar con el linkk externo al código

---
+++ commands/invitar.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const { inviteLink } = require('./commandConfig.json')
+exports.run = async (client, message, args) => {
+    let inviteChannel = message.channel;//Devuelve el canal en el cual fue enviado el mensaje.
+    let embed = new Discord.MessageEmbed()//Crea un nuevo mensaje mucho más personalizable que los mensajes base
+    .setColor('#0099ff')
+	.setTitle("Si quieres que venga más gente a jugar usa este link:")//Asigna un título
+	.setDescription(inviteLink);
+     
+    inviteChannel.send(embed);
+}

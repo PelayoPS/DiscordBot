dc887d451531df09021c443d43cb7f48a69f3c57
PelayoPS <pelayops1041@gmail.com>
2021-02-11 11:19:13 +0100

    comando decir arreglado de nuevo

--- commands/decir.js
+++ commands/decir.js
@@ -3,8 +3,12 @@ exports.run = async (client, message, args) => {
     /**
      * Comando decir que sirve para enviar un mensaje a un canal específico
      */
+    
     let channelId = args[0];
-    message.channel.id = channelId;//!hacer que el comando decir no haga crash si el formato es incorrecto
+    if(message.guild.channels.cache.get(channelId) === undefined){
+        return message.channel.send("El canal al que intentas mandar algo no existe.");
+    }
+    message.channel.id = channelId;
     let text = args.slice(1).join(" ");
     message.channel.send(text);
 

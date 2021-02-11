7bac7223a5c7f6d9b15fb9928dd8361bda1c97d1
PelayoPS <pelayops1041@gmail.com>
2021-02-11 11:15:38 +0100

    Anotaciones para mejorar ciertos comandos

--- commands/decir.js
+++ commands/decir.js
@@ -4,7 +4,7 @@ exports.run = async (client, message, args) => {
      * Comando decir que sirve para enviar un mensaje a un canal específico
      */
     let channelId = args[0];
-    message.channel.id = channelId;
+    message.channel.id = channelId;//!hacer que el comando decir no haga crash si el formato es incorrecto
     let text = args.slice(1).join(" ");
     message.channel.send(text);
 

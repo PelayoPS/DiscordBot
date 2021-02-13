6ee7b62691d62e21f0cf703c3ea1e87d3b2b5fb0
PelayoPS <pelayops1041@gmail.com>
2021-02-13 16:17:17 +0100

    arreglado el comando poll hasta que encuentre otro error

--- commands/poll.js
+++ commands/poll.js
@@ -5,14 +5,13 @@ exports.run = async (client, message, args) => {
      * se pueden poner imágenes y personalizar mucho más, pero eso lo tengo que mirar para el comando de youtube y el resto de redes
      */
     let pollChannel = message.channel;//Devuelve el canal en el cual fue enviado el mensaje.
-
     let emojis = [];
-    let i = args.length;
-    while (args[i] !== "|" || i === args.length) {
+    let i = args.indexOf("|") - 1;//se necesita saber la posición para quitar el texto de los emojis
+    while(i >= 0){
         emojis[i] = args[i];
         i--;
     }
-    let pollDescrption = args.slice(i+1).join(" ");//El formato es {formato texto sin especificar}
+    let pollDescrption = args.slice(args.indexOf("|")+1).join(" ");//El formato es {formato texto sin especificar}
     let embedPoll = new Discord.MessageEmbed()//Crea un nuevo mensaje mucho más personalizable que los mensajes base
         .setTitle("Encuesta:")//Asigna un título
         .setDescription(`<@!${message.author.id}>` + " pregunta: \n" + pollDescrption)//`<@!${message.author.id}>` sirve para hacer un @ a la persona que pone el mensaje
@@ -23,7 +22,12 @@ exports.run = async (client, message, args) => {
          * !Recordar que para hacer esto es necesario hacer la promesa o si no reacciona al mensaje anterior
          */
         for (let index = 0; index < emojis.length; index++) {
-            embedMessage.react(emojis[index]);
+            try {
+                embedMessage.react(emojis[index]);
+            } catch (error) {
+               message.channel.send("un emoji que has mandado no ha sido reconocido o hay un error en el formato") 
+            }
+            
 
         }
 

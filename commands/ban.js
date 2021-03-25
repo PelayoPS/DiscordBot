309b80e18b672d45c64c70536ff18ea94c582d9d
PelayoPS <pelayops1041@gmail.com>
2021-03-26 00:21:44 +0100

    el comando ban funciona ahora

--- commands/ban.js
+++ commands/ban.js
@@ -7,7 +7,7 @@ const Discord = require('discord.js');
  * @param {*} args sirve para sacar la razón del baneo 
  */
 exports.run = async (client, message, args) => {
-    if (!message.member.hasPermission("KICK_MEMBERS") || !message.member.hasPermission("ADMINISTRATOR")) {
+    if (!message.member.hasPermission("BAN_MEMBERS") || !message.member.hasPermission("ADMINISTRATOR")) {
         return message.channel.send("No tienes los permisos necesarios");
     }
     let user = message.mentions.users.first() || client.users.cache.get(args[0]);// guarda el usuario mencionado
@@ -16,20 +16,18 @@ exports.run = async (client, message, args) => {
     let reason = args.slice(1).join(" ");//guarda la razón de expulsión
 
     if (!user) return message.channel.send("No has mencionado a nadie");
-    if (user.id === message.author.id){
-         return message.channel.send("No te puedes banear a tí mismo camarada");
+    if (user.id === message.author.id) {
+        return message.channel.send("No te puedes banear a tí mismo camarada");
     }
     if (user.id === client.user.id) return message.channel.send("No me puedes banear jajajaaj");
 
     if (!reason) reason = "No hay razón de espulsión";
-    
-    member.send("Has sido baneado", {files: ["./fotos/ban.jpeg"]});//sends a good meme to the ban person
 
+    member.send("Has sido baneado", { files: ["./fotos/ban.jpeg"] });//sends a good meme to the ban person
 
-    member.ban(reason).then(() => {
-        message.channel.send(`Has conseguido banear a **${user.tag}**`);
-    }).catch(err => {
-        message.reply(`No he podido baneat a **${user.tag}**`);
-        console.log(err);
+    member.ban().then((member) => {
+        message.channel.send(member.displayName + " has left the server")
+        message.channel.send(member.displayName + " has been successfully banned by " +  '<@'+message.author.id+'>');
     })
+
 }
